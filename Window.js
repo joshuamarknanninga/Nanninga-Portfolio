42e8473faea6f8f3bd2a24681125003ec2601ad0
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-13 14:22:08 -0500

    debugged Window.js

--- Window.js
+++ Window.js
@@ -1,3 +1,5 @@
+// Window.js
+
 import React from 'react';
 import Draggable from 'react-draggable'; // For draggable functionality
 
@@ -7,10 +9,15 @@ function Window({ title, isMinimized, isMaximized, onClose, onMinimize, onMaximi
   };
 
   return (
-    <Draggable handle=".window-header" bounds="parent" position={position} onStop={handleDrag}>
+    <Draggable
+      handle=".window-header"
+      bounds="parent"
+      position={isMaximized ? { x: 0, y: 0 } : position}
+      onStop={handleDrag}
+    >
       <div
         className={`window bg-white border shadow-lg ${isMaximized ? 'w-screen h-screen' : 'w-80 h-auto'} 
-          ${isMinimized ? 'hidden' : 'block'} p-4 absolute`}
+          ${isMinimized ? 'hidden' : 'block'} p-4 absolute transition-all duration-300`}
       >
         <div className="window-header bg-green-500 text-white p-2 flex justify-between items-center cursor-move">
           <h2>{title}</h2>
@@ -22,7 +29,6 @@ function Window({ title, isMinimized, isMaximized, onClose, onMinimize, onMaximi
               className="w-6 h-6 cursor-pointer inline-block mx-1"
               onClick={onMinimize}
             />
-
             {/* Maximize Button */}
             <img
               src="https://cdn.iconfinder.com/data/icons/feather/24/maximize-512.png" // Maximize Icon URL
@@ -30,7 +36,6 @@ function Window({ title, isMinimized, isMaximized, onClose, onMinimize, onMaximi
               className="w-6 h-6 cursor-pointer inline-block mx-1"
               onClick={onMaximize}
             />
-
             {/* Close Button */}
             <img
               src="https://cdn.iconfinder.com/data/icons/feather/24/x-512.png" // Close Icon URL
