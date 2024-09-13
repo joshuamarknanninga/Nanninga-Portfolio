9bbbecbb2d38699119e7b1964a2d0efdd9e08a39
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-13 14:34:07 -0500

    added framer-motion, updated window.js

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,7 +1,8 @@
 // Window.js
 
 import React from 'react';
-import Draggable from 'react-draggable'; // For draggable functionality
+import Draggable from 'react-draggable';
+import { motion } from 'framer-motion';
 
 function Window({ title, isMinimized, isMaximized, onClose, onMinimize, onMaximize, position, onMove, children }) {
   const handleDrag = (e, data) => {
@@ -15,30 +16,34 @@ function Window({ title, isMinimized, isMaximized, onClose, onMinimize, onMaximi
       position={isMaximized ? { x: 0, y: 0 } : position}
       onStop={handleDrag}
     >
-      <div
+      <motion.div
         className={`window bg-white border shadow-lg ${isMaximized ? 'w-screen h-screen' : 'w-80 h-auto'} 
           ${isMinimized ? 'hidden' : 'block'} p-4 absolute transition-all duration-300`}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.3 }}
       >
         <div className="window-header bg-green-500 text-white p-2 flex justify-between items-center cursor-move">
           <h2>{title}</h2>
           <div>
             {/* Minimize Button */}
             <img
-              src="https://cdn.iconfinder.com/data/icons/feather/24/minimize-512.png" // Minimize Icon URL
+              src="https://cdn.iconfinder.com/data/icons/feather/24/minimize-512.png"
               alt="Minimize"
               className="w-6 h-6 cursor-pointer inline-block mx-1"
               onClick={onMinimize}
             />
             {/* Maximize Button */}
             <img
-              src="https://cdn.iconfinder.com/data/icons/feather/24/maximize-512.png" // Maximize Icon URL
+              src="https://cdn.iconfinder.com/data/icons/feather/24/maximize-512.png"
               alt="Maximize"
               className="w-6 h-6 cursor-pointer inline-block mx-1"
               onClick={onMaximize}
             />
             {/* Close Button */}
             <img
-              src="https://cdn.iconfinder.com/data/icons/feather/24/x-512.png" // Close Icon URL
+              src="https://cdn.iconfinder.com/data/icons/feather/24/x-512.png"
               alt="Close"
               className="w-6 h-6 cursor-pointer inline-block mx-1"
               onClick={onClose}
@@ -48,7 +53,7 @@ function Window({ title, isMinimized, isMaximized, onClose, onMinimize, onMaximi
         <div className="window-content p-4">
           {children}
         </div>
-      </div>
+      </motion.div>
     </Draggable>
   );
 }
